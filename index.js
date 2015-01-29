c1271cf81356953f5e7143de47ce347d3771e9a7
Bálder Carraté <balder.carrate@otium360.com>
2015-01-29 21:07:07 +0100

    Update nodemon configuration

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ var noderest = require('./lib/core')
 
 noderest.bootstrap()
 
-process.on('SIGINT', function() {
+var shutdown = function () {
   noderest.shutdown(function () {
     process.exit(0)
   })
-})
+};
+
+process.on('SIGINT', shutdown)  // ctrl-C
+process.on('SIGUSR2', shutdown) // nodemon restart
